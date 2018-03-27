fe47d2e9272d7b73adbfc2cce176cced4755d8fb
Diego Martins de Oliveira <diego.martins@latitude.unb.br>
2018-03-26 23:58:28 -0300

    Visto1

---
+++ Visto1/Debug/main.d
@@ -0,0 +1,15 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /home/diego/ti/ccs740/ccsv7/ccs_base/msp430/include/msp430.h
+main.obj: /home/diego/ti/ccs740/ccsv7/ccs_base/msp430/include/msp430f5529.h
+main.obj: /home/diego/ti/ccs740/ccsv7/ccs_base/msp430/include/in430.h
+main.obj: /home/diego/ti/ccs740/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics.h
+main.obj: /home/diego/ti/ccs740/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics_legacy_undefs.h
+
+../main.c: 
+/home/diego/ti/ccs740/ccsv7/ccs_base/msp430/include/msp430.h: 
+/home/diego/ti/ccs740/ccsv7/ccs_base/msp430/include/msp430f5529.h: 
+/home/diego/ti/ccs740/ccsv7/ccs_base/msp430/include/in430.h: 
+/home/diego/ti/ccs740/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics.h: 
+/home/diego/ti/ccs740/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics_legacy_undefs.h: 
